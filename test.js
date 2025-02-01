908ab81089cb3bccf101057f8bffcdda5e565c99
kittanai <kittanai_yam@cmu.ac.th>
2025-02-01 15:57:47 +0700

    Remove context in test and change into if-else

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-console.log('Hello World!')
-
-x = 5
-y = 6
-z = x + y
-console.log
+let x = 55
+if (x > 80) {
+    console.log('Grade: A')
+} else if (x > 60) {
+    console.log('Grade: B')
+}  else if (x > 50) {
+    console.log('Grade: C')
+}   else {
+    console.log('Grade: D')
+}
